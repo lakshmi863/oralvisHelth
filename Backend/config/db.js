43e7c4c57b3d9bc46eceb3ab3fc3556f8289ee2c
lakshmi863 <sathya.lakshmipathi@gmail.com>
2025-09-02 10:26:58 +0530

    sqliteadd

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,9 +1,6 @@
-import sqlite3 from "sqlite3";
-import { open } from "sqlite";
+import Database from "better-sqlite3";
 
 export const initDB = async () => {
-  return open({
-    filename: "./oralvis.db",
-    driver: sqlite3.Database,
-  });
+  const db = new Database("./oralvis.db");
+  return db;
 };
