b807ac88958f44a1696e5cee842773e1de268bab
lakshmi863 <sathya.lakshmipathi@gmail.com>
2025-09-02 17:01:22 +0530

    path add

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:4000/api",
+  baseURL: process.env.REACT_APP_API_URL || "https://oralvishelth-backend-demo.onrender.com",
 });
 
 // Attach JWT if exists
